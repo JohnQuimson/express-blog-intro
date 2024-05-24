d4f173d9b96f39551863f9debdb9e5495cb124a4
JohnQuimson <john.quimson777@gmail.com>
2024-05-24 14:48:21 +0200

    feat: controllers

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Benvenuto nel mio blog!<h1/>');
+const { index } = require('./controllers/index.js');
+const { post } = require('./controllers/post.js');
+
+app.get('/', index);
+
+app.get('/posts', (req, res) => {
+  res.send('<ul><ul/>');
 });
 
 app.listen(port, () => {
---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  index: (req, res) => {
+    res.send('<h1>Benvenuto nel mio blog!<h1/>');
+  },
+};
