87ed54cb17e2f499539d30d48e40ec8516963d7f
JohnQuimson <john.quimson777@gmail.com>
2024-05-24 16:57:58 +0200

    fix: utils path

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 app.use(express.static('public'));
 app.use(express.json());
-const { writeJson } = require('../utils');
+const { writeJson } = require('./utils');
 
 const { index } = require('./controllers/index.js');
 const { posts } = require('./controllers/posts.js');
@@ -11,13 +11,10 @@ const { posts } = require('./controllers/posts.js');
 app.get('/', index);
 app.get('/posts', posts);
 
-// app.post('/posts', (req, res) => {
-//   writeJson('posts', req.body);
-//   res.send('Post effettuato correttamente');
-// });
-// app.get('/posts', (req, res) => {
-//   res.send('<ul><ul/>');
-// });
+app.post('/posts', (req, res) => {
+  writeJson('posts', [...post, req.body]);
+  res.send('Post effettuato correttamente');
+});
 
 app.listen(port, () => {
   console.log(`Server avviato su http://localhost:${port}`);
